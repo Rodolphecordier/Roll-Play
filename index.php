3700b5427642546dd526a423e355665e97258dcd
Rodolphecordier <32874856+Rodolphecordier@users.noreply.github.com>
2018-03-20 17:26:51 +0100

    roll&play

---
+++ index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>La mère à lucien</title>
+
+    <!-- Pour Internet Explorer : S'assurer qu'il utilise la dernière version du moteur de rendu -->
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Affichage sans zoom pour les mobiles -->
+    <meta name="viewport" content="width=device-width, init-scale=1">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Styles CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!--  Font Awesome | Icons -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    <!-- HTML5 Shiv & Respond JS -->
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" integrity="sha256-3Jy/GbSLrg0o9y5Z5n1uw0qxZECH7C6OQpVBgNFYa0g=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js" integrity="sha256-g6iAfvZp+nDQ2TdTR/VVKJf3bGro4ub5fvWSWVRi2NE=" crossorigin="anonymous"></script>
+    <![endif]-->
+
+
+</head>
+<body>
+<nav class="navbar">
+    <div class="container">
+        <ul class="navbar1">
+            <li><a class="white" href="#">Inscription</a></li>
+            <li><a class="white" href="#">Login</a></li>
+        </ul>
+    </div>
+</nav>
+<div class="container-full">
+    <div class="row" style="width: 100%">
+        <div class="col-xs-12">
+            <div class="col-sm-5">
+                <div class="slogan">
+                    <h3>  Roll and play make your own world</h3>
+                </div>
+
+            </div>
+            <div class="col-sm-2">
+
+            </div>
+            <div class="col-sm-5">
+                <div class="actu">
+                    <h4>Nouvel univers</h4>
+                    <p>L'univers de ctuhllu est maintenant disponible !!!</p>
+                    <p>Auteur: Riuki<br>
+                        Date: 20/03/2018</p>
+                    <hr>
+                    <h4>Nouveaux venus</h4>
+                    <p>Bienvenue a XX--Dark--Hugo--XX</p>
+                    <p>Auteur: Riuki<br>
+                        Date: 20/03/2018</p>
+                    <hr>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="univers container-fluid">
+        <div class="row" style="width: 100%">
+            <div class="col-sm-12 univer ">
+<h1>Univers:</h1>
+                <div class="universcthulhu col-sm-5">
+                    <h5>l'appel de Cthulhu</h5>
+                    <p>Cthulhu est une créature fantastique de fiction tirée de l'œuvre littéraire de l'écrivain américain Howard Phillips Lovecraft.<br>
+                        Il apparaît pour la première fois dans la nouvelle L'Appel de Cthulhu, publiée en 1928.<br>
+
+                        Gigantesque monstre extraterrestre,<br>
+                        Cthulhu possède une tête de seiche ainsi que des tentacules de pieuvre et des ailes semblables à celles d'un dragon.<br>
+                        Certains humains dévoyés lui vouent un culte immémorial.</p>
+                </div>
+
+
+                <div class="universagone col-sm-5">
+                    <h5>Agone</h5>
+                    <p>
+                        Agone est un jeu de rôle sur table français médiéval-fantastique aux inspirations baroques publié en 1999 chez Multisim. Il est basé sur la trilogie des Chroniques des Crépusculaires,<br>
+                        Les personnages des joueurs, appelés les Inspirés, luttent contre l'influence pernicieuse du Masque, un être manipulateur qui cherche à plonger le monde dans la perfidie et l'obscurantisme.<br>
+                        De nombreux peuples, royaumes, factions et formes de magie sont proposées.</p>
+                </div>
+
+            </div>
+        </div>
+        <div class="row" style="width: 100%">
+            <div class="col-sm-12 univer">
+
+                <div class="universYggdrasill col-sm-5">
+                    <h5>Yggdrasill </h5>
+                    <p>Yggdrasill, Les Terres du Nord, vous permet de vivre des aventures palpitantes dans un cadre dense et fascinant,<br>
+                        et de devenir ainsi un héros dans cette Scandia encore légendaire. Intrigues, combats, quêtes, magie et créatures mythiques vous y attendent.<br>
+                        Héros en quête d'aventure, de gloire ou d'immortalité, vous prendrez part aux grandes batailles qui mèneront des plaines enneigées du Jylland à la cour des rois,et peut-être même, héros reconnu par Odhinn, le Redoutable,<br>
+
+
+                    </p>
+                </div>
+
+
+                <div class="universmutant col-sm-5">
+                    <h5>Mutant - Année Zéro</h5>
+                    <p>Vous êtes les survivants de l'effondrement du monde. Vous êtes les habitants dans l'Arche, un fragile bastion en bordure d'une cité morte.<br>
+                        Vous êtes les rejetons de l'humanité, mais vous n'êtes plus humains. Vous en êtes les reflets déformés, les mutants du Nouveau Monde en quête de l'Éden.
+
+                    </p>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+    <h1>Contact:</h1>
+    <div class="contact container-fluid">
+
+        <form id="contact-form" method="post" action="#" class="contact-form form">
+            <div class="controls">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label  for="name">Votre prenom *</label>
+                            <input type="text" name="name" id="name" placeholder="Entrez votre Prénom" required="required" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label  for="surname">Votre Nom *</label>
+                            <input type="text" name="surname" id="surname" placeholder="Entrez votre Nom" required="required" class="form-control">
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label  for="email">Votre email *</label>
+                    <input type="email" name="email" id="email" placeholder="Entrez votre email" required="required" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label  for="message">Votre message *</label>
+                    <textarea rows="4" name="message" id="message" placeholder="Entrez votre message" required="required" class="form-control"></textarea>
+                </div>
+                <div class="text-center">
+                    <input name="contact" type="submit" value="Envoyez message" class="btn btn-primary btn-block">
+                </div>
+            </div>
+    </div>
+    </form>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"></script>
+<script rel="stylesheet" src="assets/js/script.js"></script>
+</body>
+</html>
